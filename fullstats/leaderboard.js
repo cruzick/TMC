14433a07f520b772b0bc6af0c7fe3c0a0b1b835a
Cruzick <cpierce515@gmail.com>
2022-01-17 22:37:28 -0600

    Update leaderboard.js

--- fullstats/leaderboard.js
+++ fullstats/leaderboard.js
@@ -8,8 +8,8 @@ async function run() {
     let resultField = document.getElementById("resultField");
     resultField.textContent = "Loading...";
     totalmined = 0;
-    const from = new Date("2022-01-13T00:00:00Z");
-    const to = new Date("2022-01-13T23:59:59Z")
+    const from = new Date("2022-01-12T00:00:00Z");
+    const to = new Date("2022-01-12T23:59:59Z")
     let results = await downloadData(from, to);
     let usedResultsCnt = 0;
 
