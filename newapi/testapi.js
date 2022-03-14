867f45f348bd65c6a9891e56f815f79330dd09d2
Cruzick <cpierce515@gmail.com>
2022-03-14 13:34:33 -0500

    Update testapi.js

--- newapi/testapi.js
+++ newapi/testapi.js
@@ -35,7 +35,7 @@ async function run() {
         for (let i = 0; i < results.length; i++)   {        
             //adds up commission
             commission = results[i].act.data.amount + commission;
-            console.log(results[i].act.data.amount + " " + i + " " + results[i].timestamp);
+            console.log(results[i].act.data.amount + " " + i + " " + results[i].act.timestamp);
         }
 
         resultField.textContent =  "Total Commission: " + commission.toFixed(4);
